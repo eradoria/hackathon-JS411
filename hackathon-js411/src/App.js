87ed063270fc97e0fec7421dbe4b75ee30f36ae6
JTDoe <Jordendoerflinger@gmail.com>
2022-08-04 20:36:50 -0500

    jordens commit

--- hackathon-js411/src/App.js
+++ hackathon-js411/src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <header>
+    <h2>Search Hacker News</h2>
+    <input></input>
+    <h2>Settings</h2>
+    </header>
+    <body>
+
+    </body>
+  </div>;
+  
 }
 
 export default App;
